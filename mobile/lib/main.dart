99bdd2e480c39beaa953f76ad78404b7b4b1ff40
mauryasujeet698-svg <mauryasujeet698@gmail.com>
2026-09-20 11:40:36 +0530

    Initialize Firebase with ALLways Android configuration

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -2,10 +2,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const AllwaysApp());
 }
 

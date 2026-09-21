02144c54419807d03f6306921765223d00991aa6
mauryasujeet698-svg <mauryasujeet698@gmail.com>
2026-09-21 12:07:42 +0530

    Add Dio dependency import for in-app updater

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
